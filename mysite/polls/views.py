84bd4c3baec8ecc5a409c9f666d02fd1e1fab6eb
yanesss <aaronc.yanes@calbaptist.edu>
2018-12-04 12:15:53 -0800

    lastest

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template("/Users/aaronyanes/tweetMap/index.html")
+    return HttpResponse(template.render())
+
+
+
+
+    
